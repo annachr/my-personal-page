a8a538c0a8ba432521e828014a430f945d124673
Ania <anna.ananicz@gmail.com>
2021-05-26 14:27:25 +0200

    modified effects on displaying resume sections after clicking on headers

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 
 const Features = (props) => {
   const [display, setDisplay] = useState(false)
+  const [headerActive, setHeaderActive] = useState('')
   function handleDisplay() {
     setDisplay(!display)
+    !display ? setHeaderActive('active') : setHeaderActive('')
   }
 
   return (
-    <div>
-      <h3 onClick={handleDisplay}>{props.header}</h3>
+    <div className='feature'>
+      <h3 onClick={handleDisplay} className={headerActive}>{props.header}</h3>
       <div style={{display: display ? 'block' : 'none'}} className='resumeData'>{props.content}</div>
     </div>
   )
