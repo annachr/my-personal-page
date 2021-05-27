7fb826ad98e3461b020e9227f15cdb5da8b1177e
Ania <anna.ananicz@gmail.com>
2021-05-27 16:08:40 +0200

    deleted unused redux; added 'move to top' button in the footer; changed background color of header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <h4>Contact me</h4>
-      <p>e-mail: anna.ananicz@gmail.com</p>
-      mobile: +48 664 415 169
+      <div>
+        <h4>Contact me</h4>
+        <p>e-mail: anna.ananicz@gmail.com</p>
+        <p>mobile: +48 664 415 169</p>
+      </div>
+      <a href='#home' className='arrowBtn'>»</a>
     </footer>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import linkedIn from '../static/linkedIn-icon.svg'
 const Header = () => {
   return (
     <div>
-      <header>
+      <header id='home'>
         <h1>Hi, I'm Anna</h1>
         <div className='socialIcons'>
           <a href='https://github.com/annachr?tab=repositories'><img className='github icon' src={githubIcon} alt='github icon'/></a>
@@ -13,7 +13,7 @@ const Header = () => {
         </div>
       </header>
       <p className='about'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio.</p>
-      {/*<button className='arrowBtn'>»</button>*/}
+
     </div>
   )
 }
